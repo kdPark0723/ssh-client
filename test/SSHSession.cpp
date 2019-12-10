a7990e70354d858b00c33438e16f8b0444463560
kdpark0723 <kdpark0723@gmail.com>
2019-12-10 21:34:45 +0900

    out-of-memory

--- test/SSHSession.cpp
+++ test/SSHSession.cpp
@@ -1,6 +1,18 @@
 #include "SSHSession.h"
 #include "CantCreateSSHSesstionException.h"
 
+#include <libssh/libssh.h>
+
+void throwErrorIfNotZero(int code, const std::string &message = "code is not zero") {
+    if (code != 0)
+        throw CantCreateSSHSesstionException(message);
+}
+
+void throwErrorIfNotOK(int code, ssh_session session) {
+    if (code != SSH_OK)
+        throw CantCreateSSHSesstionException(ssh_get_error(session));
+}
+
 SSHSession::SSHSession(const SSHInfo &info)
     : session{ ssh_new()},
     is_connecting{ false }
@@ -8,10 +20,26 @@ SSHSession::SSHSession(const SSHInfo &info)
     if (session == nullptr)
         throw CantCreateSSHSesstionException("session should not null");
     
-    ssh_options_set(session, SSH_OPTIONS_HOST, info.host.c_str());
-    ssh_options_set(session, SSH_OPTIONS_PORT, &info.port);
-    if (!info.user.empty())
-        ssh_options_set(session, SSH_OPTIONS_USER, info.user.c_str());
+    throwErrorIfNotOK(
+        ssh_options_set(session, SSH_OPTIONS_HOST, info.host.c_str()),
+        session
+    );
+    throwErrorIfNotOK(
+        ssh_options_set(session, SSH_OPTIONS_PORT, &info.port),
+        session
+    );
+
+    // If it removed, connect happen out-of memory.
+    throwErrorIfNotOK(
+        ssh_options_set(session, SSH_OPTIONS_SSH_DIR, nullptr),
+        session
+    );
+    if (!info.user.empty()) {
+        throwErrorIfNotOK(
+            ssh_options_set(session, SSH_OPTIONS_USER, info.user.c_str()),
+            session
+        );
+    }
 }
 
 SSHSession::~SSHSession()
@@ -25,13 +53,13 @@ void SSHSession::connect()
 {
     is_connecting = true;
 
-    auto rc{ ssh_connect(session) };
-    if (rc != SSH_OK)
-        throw CantCreateSSHSesstionException(ssh_get_error(session));
+    throwErrorIfNotOK(ssh_connect(session), session);
 }
 
 void SSHSession::disconnect()
 {
     if (is_connecting)
         ssh_disconnect(session);
+
+    is_connecting = false;
 }
